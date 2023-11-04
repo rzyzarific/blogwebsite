ab14a865fda2adae582db135f9ec04e05ed31adf
Zarif Yeameen <114343857+rzyzarific@users.noreply.github.com>
2023-11-04 14:08:22 +0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+
+app = Flask(__name__, static_folder='assets')
+
+@app.route('/')
+def index():
+   return render_template('index.html')
+
+@app.route('/blog')
+def blog():
+   return render_template('blog.html')
+
+@app.route('/contact')
+def contact():
+   return render_template('contact.html')
+
+if __name__ == '__main__':
+   app.run(debug=True)
